0b30761326988aad698a68627dbd916e6d495d96
Luktoria <110913006+Luktoria@users.noreply.github.com>
2023-09-07 22:02:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+/*Nav listeners*/
+
+document.getElementById('linkedin-nav-icon').addEventListener("mouseover", ()=>{document.getElementById('linkedin-nav-icon').src = "./images/linkedin-white.svg"});
+document.getElementById('linkedin-nav-icon').addEventListener("mouseleave", ()=>{document.getElementById('linkedin-nav-icon').src = "./images/linkedin-beige.svg"});
+
+document.getElementById('envelope').addEventListener("mouseover", ()=>{document.getElementById('envelope').src = "./images/envelope-white.svg"});
+document.getElementById('envelope').addEventListener("mouseleave", ()=>{document.getElementById('envelope').src = "./images/envelope-beige.svg"});
+
+document.getElementById('github-js').addEventListener("mouseover", ()=>{document.getElementById('github-js').src = "./images/github-white.svg"});
+document.getElementById('github-js').addEventListener("mouseleave", ()=>{document.getElementById('github-js').src = "./images/github-beige.svg"});
+
